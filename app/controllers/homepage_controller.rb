022704f7af0a91dc1c976bf301aed341bccc4007
Myles <mylesdawson@gmail.com>
2017-03-19 00:01:08 -0700

    adding controller for database

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -5,6 +5,28 @@ class HomepageController < ApplicationController
   end
   def stats
   end
+
+  private
+
+
+  #   These following functions depends on user who logged in
+
+  def get_fuel_efficiency
+    @fuel = 5;
+  end
+
+  def get_saved_money
+
+  end
+
+  def distance_traveled_this_week
+
+  end
+
+  def emissions
+
+  end
+
 end
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -55,6 +55,8 @@ ActiveRecord::Schema.define(version: 20170319060300) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name",                          null: false
     t.string   "last_name",                           null: false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
