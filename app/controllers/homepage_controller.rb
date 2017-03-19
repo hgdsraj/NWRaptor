7d94b55bd86573d4cd1d93b728d4aa17c599a609
Myles <mylesdawson@gmail.com>
2017-03-19 05:59:03 -0700

    minor to stats page

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,28 +4,8 @@ class HomepageController < ApplicationController
   def nearby
   end
   def stats
-	  get_fuel_efficiency
   end
 
-  private
-
-
-  #   These following functions depends on user who logged in
-
-  def get_fuel_efficiency
-  end
-
-  def get_saved_money
-
-  end
-
-  def distance_traveled_this_week
-
-  end
-
-  def emissions
-
-  end
 
 end
 
